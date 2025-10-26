e84f7e70183ac93cea12bb09893e93ede2d751bc
Balaji <bmn636169@gmail.com>
2025-10-26 19:06:32 +0530

    Initial commit

---
+++ app/src/main/java/com/app/zepto/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.app.zepto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
